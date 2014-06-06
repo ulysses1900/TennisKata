d49b62bad4b9f4dec2cfe1379db9e0b73ddeb4a6
ulysses1900 <moskalski99@gmail.com>
2014-06-06 12:16:59 +0200

    WIP basic tests done

--- src/main/Tennis.java
+++ src/main/Tennis.java
@@ -3,36 +3,57 @@ package main;
 public class Tennis {
 	public Player one;
 	public Player two;
-	
-	class Player{
+
+	public enum Score {
+		ZERO, FIFTEEN, THIRTY, FOURTY, DOUCE, ADVANTAGE, WINS
+	}
+
+	class Player {
 		int playerNumber;
-		int currentScore;
-		
-		public int getCurrentScore(){
+		Score currentScore;
+
+		public Score getCurrentScore() {
 			return currentScore;
 		}
-		
-		Player (int number, int score){
+
+		Player(int number) {
 			playerNumber = number;
-			currentScore = score;
+			currentScore = Score.ZERO;
 		}
-		
-		public void addPoints(){
-			if (currentScore < 30) currentScore += 15;
-			if (currentScore >= 30) currentScore += 10;
+
+		public void addPoints() {
+			if (currentScore == Score.ZERO)
+				currentScore = Score.FIFTEEN;
+			else if (currentScore == Score.FIFTEEN)
+				currentScore = Score.THIRTY;
+			else if (currentScore == Score.THIRTY)
+				currentScore = Score.FOURTY;
+			else if (currentScore == Score.FOURTY)
+				currentScore = Score.WINS;
 		}
 	}
-	
-	public Tennis (){
-		one = new Player(1, 0);
-		two = new Player(2, 0);
+
+	public Tennis() {
+		one = new Player(1);
+		two = new Player(2);
 	}
-	
-	public void score(Player player){
-		player.addPoints();
+
+	public void score(int playerNumber) {
+		if (playerNumber == 1) {
+			one.addPoints();
+		} else if (playerNumber == 2) {
+			two.addPoints();
+		} else
+			throw new IllegalArgumentException(
+					"Only \"1\" or \"2\" is allowed!");
 	}
-	
-	public String getScore(){
+
+	public String getScore() {
+		if (one.getCurrentScore() == Score.FOURTY
+				&& two.getCurrentScore() == Score.FOURTY) {
+			return Score.DOUCE.toString();
+		}
 		return one.getCurrentScore() + "-" + two.getCurrentScore();
+
 	}
 }
--- src/test/TennisTests.java
+++ src/test/TennisTests.java
@@ -1,30 +1,57 @@
 package test;
 
-import main.Tennis;
-
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
+import main.Tennis;
 
 public class TennisTests {
 	@Test
 	public void zeroZeroReturnsZeroZero() {
 		Tennis tennis = new Tennis();
 
-		Assert.assertEquals(tennis.getScore(), "0-0");
+		AssertJUnit.assertEquals(tennis.getScore(), "ZERO-ZERO");
 	}
 
 	@Test
 	public void playerOneFirstScore() {
 		Tennis tennis = new Tennis();
-		tennis.score(tennis.one);
-		Assert.assertEquals(tennis.getScore(), "15-0");
+		tennis.score(1);
+		AssertJUnit.assertEquals(tennis.getScore(), "FIFTEEN-ZERO");
 	}
-	
+
 	@Test
 	public void playerOneSecondScore() {
 		Tennis tennis = new Tennis();
-		tennis.score(tennis.one);
-		tennis.score(tennis.one);
-		Assert.assertEquals(tennis.getScore(), "30-0");
+		tennis.score(1);
+		tennis.score(1);
+		AssertJUnit.assertEquals(tennis.getScore(), "THIRTY-ZERO");
 	}
+
+	@Test(expectedExceptions = IllegalArgumentException.class)
+	public void wrongPlayerNumberReturnsException() {
+		Tennis tennis = new Tennis();
+		tennis.score(0);
+	}
+
+	@Test
+	public void playerOneWins() {
+		Tennis tennis = new Tennis();
+		for (int i = 0; i < 5; i++) {
+			tennis.score(1);
+		}
+		AssertJUnit.assertEquals(tennis.getScore(), "WINS-ZERO");
+	}
+	
+	@Test
+	public void deuce() {
+		Tennis tennis = new Tennis();
+		for (int i = 0; i < 3; i++) {
+			tennis.score(1);
+			tennis.score(2);
+			System.out.println(tennis.getScore());
+		}
+		AssertJUnit.assertEquals(tennis.getScore(), "DOUCE");
+	}
+	
+	
 }
