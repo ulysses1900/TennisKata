baec3e75a90a58210b29ba4a6405aab22d700966
ulysses1900 <moskalski99@gmail.com>
2014-06-06 22:07:56 +0200

    Final commit. Refactoring is done. Test are all OK.

--- src/main/Tennis.java
+++ src/main/Tennis.java
@@ -1,59 +1,131 @@
 package main;
 
 public class Tennis {
-	public Player one;
-	public Player two;
-
-	public enum Score {
-		ZERO, FIFTEEN, THIRTY, FOURTY, DOUCE, ADVANTAGE, WINS
-	}
-
-	class Player {
-		int playerNumber;
-		Score currentScore;
-
-		public Score getCurrentScore() {
-			return currentScore;
-		}
-
-		Player(int number) {
-			playerNumber = number;
-			currentScore = Score.ZERO;
-		}
-
-		public void addPoints() {
-			if (currentScore == Score.ZERO)
-				currentScore = Score.FIFTEEN;
-			else if (currentScore == Score.FIFTEEN)
-				currentScore = Score.THIRTY;
-			else if (currentScore == Score.THIRTY)
-				currentScore = Score.FOURTY;
-			else if (currentScore == Score.FOURTY)
-				currentScore = Score.WINS;
-		}
-	}
+	private int playerOneScore;
+	private int playerTwoScore;
+	private String[][] Score;
+	private String errMsg = "Only \"1\" or \"2\" is allowed!";
+	private String gaveOverMsg = "Game is over.";
 
 	public Tennis() {
-		one = new Player(1);
-		two = new Player(2);
+		Score = new String[2][8];
+		initScore();
+		playerOneScore = 0;
+		playerTwoScore = 0;
 	}
 
-	public void score(int playerNumber) {
-		if (playerNumber == 1) {
-			one.addPoints();
-		} else if (playerNumber == 2) {
-			two.addPoints();
+	private void initScore() {
+		for (int i = 0; i < 2; i++) {
+			Score[i][0] = "0";
+			Score[i][1] = "15";
+			Score[i][2] = "30";
+			Score[i][3] = "40";
+			Score[i][4] = "Win";
+			Score[i][5] = "Advantage";
+			Score[i][6] = "Deuce";
+			Score[i][7] = "";
+		}
+	}
+
+	public void score(int player) {
+		if (!isGameOver()) {
+			switch (player) {
+			case 1:
+				if (isDeuce())
+					setAdvantage(1);
+				else if (isAdvantage(2))
+					setDeuce();
+				else if (isAdvantage(1))
+					setWin(1);
+				else
+					addScore(1);
+				break;
+			case 2:
+				if (isDeuce())
+					setAdvantage(2);
+				else if (isAdvantage(1))
+					setDeuce();
+				else if (isAdvantage(2))
+					setWin(2);
+				else
+					addScore(2);
+				break;
+			default:
+				throw new IllegalArgumentException(errMsg);
+			}
 		} else
-			throw new IllegalArgumentException(
-					"Only \"1\" or \"2\" is allowed!");
+			System.out.println(gaveOverMsg);
+	}
+
+	public void addScore(int player) {
+		switch (player) {
+		case 1:
+			playerOneScore++;
+			break;
+		case 2:
+			playerTwoScore++;
+			break;
+		default:
+			throw new IllegalArgumentException(errMsg);
+		}
 	}
 
 	public String getScore() {
-		if (one.getCurrentScore() == Score.FOURTY
-				&& two.getCurrentScore() == Score.FOURTY) {
-			return Score.DOUCE.toString();
-		}
-		return one.getCurrentScore() + "-" + two.getCurrentScore();
+		if (isDeuce())
+			return "Deuce";
+		return Score[0][playerOneScore] + "-" + Score[1][playerTwoScore];
+	}
 
+	public boolean isDeuce() {
+		return ((playerOneScore == playerTwoScore) && ((playerOneScore == 6) || (playerOneScore == 3)));
+	}
+
+	public void setDeuce() {
+		playerOneScore = playerTwoScore = 6;
+	}
+
+	public boolean isAdvantage(int player) {
+		switch (player) {
+		case 1:
+			return (playerOneScore == 5 && playerTwoScore == 7);
+		case 2:
+			return (playerOneScore == 7 && playerTwoScore == 5);
+		default:
+			throw new IllegalArgumentException(errMsg);
+		}
+	}
+
+	public void setAdvantage(int player) {
+		switch (player) {
+		case 1:
+			playerOneScore = 5;
+			playerTwoScore = 7;
+			break;
+		case 2:
+			playerOneScore = 7;
+			playerTwoScore = 5;
+			break;
+		default:
+			throw new IllegalArgumentException(errMsg);
+		}
+	}
+
+	public void setWin(int player) {
+		switch (player) {
+		case 1:
+			playerOneScore = 4;
+			playerTwoScore = 7;
+			break;
+		case 2:
+			playerOneScore = 7;
+			playerTwoScore = 4;
+			break;
+		default:
+			throw new IllegalArgumentException(errMsg);
+		}
+	}
+
+	public boolean isGameOver() {
+		return (playerOneScore == 4 || playerTwoScore == 4);
 	}
 }
--- src/test/TennisTests.java
+++ src/test/TennisTests.java
@@ -1,22 +1,23 @@
 package test;
 
-import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
 import main.Tennis;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TennisTests {
 	@Test
 	public void zeroZeroReturnsZeroZero() {
 		Tennis tennis = new Tennis();
 
-		AssertJUnit.assertEquals(tennis.getScore(), "ZERO-ZERO");
+		Assert.assertEquals(tennis.getScore(), "0-0");
 	}
 
 	@Test
 	public void playerOneFirstScore() {
 		Tennis tennis = new Tennis();
 		tennis.score(1);
-		AssertJUnit.assertEquals(tennis.getScore(), "FIFTEEN-ZERO");
+		Assert.assertEquals(tennis.getScore(), "15-0");
 	}
 
 	@Test
@@ -24,7 +25,9 @@ public class TennisTests {
 		Tennis tennis = new Tennis();
 		tennis.score(1);
 		tennis.score(1);
-		AssertJUnit.assertEquals(tennis.getScore(), "THIRTY-ZERO");
+		tennis.score(2);
+		tennis.score(2);
+		Assert.assertEquals(tennis.getScore(), "30-30");
 	}
 
 	@Test(expectedExceptions = IllegalArgumentException.class)
@@ -34,24 +37,66 @@ public class TennisTests {
 	}
 
 	@Test
-	public void playerOneWins() {
+	public void playerOneWin() {
 		Tennis tennis = new Tennis();
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < 4; i++) {
 			tennis.score(1);
 		}
-		AssertJUnit.assertEquals(tennis.getScore(), "WINS-ZERO");
+		Assert.assertEquals(tennis.getScore(), "Win-0");
 	}
-	
+
+	@Test
+	public void playerTwoWin() {
+		Tennis tennis = new Tennis();
+		for (int i = 0; i < 4; i++) {
+			tennis.score(2);
+		}
+		Assert.assertEquals(tennis.getScore(), "0-Win");
+	}
+
 	@Test
 	public void deuce() {
+		Tennis tennis = new Tennis();
+		for (int i = 0; i < 4; i++) {
+			tennis.score(1);
+			tennis.score(2);
+		}
+		Assert.assertEquals(tennis.getScore(), "Deuce");
+	}
+
+	@Test
+	public void advantage() {
 		Tennis tennis = new Tennis();
 		for (int i = 0; i < 3; i++) {
 			tennis.score(1);
 			tennis.score(2);
-			System.out.println(tennis.getScore());
 		}
-		AssertJUnit.assertEquals(tennis.getScore(), "DOUCE");
+		tennis.score(1);
+		Assert.assertEquals(tennis.getScore(), "Advantage-");
 	}
-	
-	
+
+	@Test
+	public void advantagePlayerTwo() {
+		Tennis tennis = new Tennis();
+		for (int i = 0; i < 3; i++) {
+			tennis.score(1);
+			tennis.score(2);
+		}
+		tennis.score(2);
+		Assert.assertEquals(tennis.getScore(), "-Advantage");
+	}
+
+	@Test
+	public void manyDeucesOneWin() {
+		Tennis tennis = new Tennis();
+		for (int i = 0; i < 7; i++) {
+			tennis.score(1);
+			tennis.score(2);
+		}
+		tennis.score(1);
+		tennis.score(1);
+		tennis.score(1);
+		Assert.assertEquals(tennis.getScore(), "Win-");
+	}
+
 }
